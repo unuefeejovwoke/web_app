4a6238e56277ee773e486f0c64af84185064216b
unuefeejovwoke <vuwill7114@gmail.com>
2021-02-12 18:22:36 -0800

    updated website

--- js/main.js
+++ js/main.js
@@ -302,16 +302,7 @@
 
 
 //*---hide all section except active**
-(() =>{
 
-  const sections = document.querySelectorAll(".section");
-  sections.forEach((section) =>{
-    if(!section.classList.contains("active")){
-      section.classList.add("hide");
-    }
-  });
-
-})();
 
  
  window.addEventListener("load", () =>{
